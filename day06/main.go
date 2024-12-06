1fe033f5286b2f641247a92dbea452d0aaf26f0a
Michael Järvå <jarva@Michaels-MacBook-Air.local>
2024-12-06 11:39:28 +0100

    Dry Day06

--- day06/main.go
+++ day06/main.go
@@ -7,60 +7,27 @@ import (
 	"os"
 )
 
+var OBSTACLE rune = '#'
+var EMPTY rune = '.'
+var VISITED rune = 'X'
+
 func solvePart1(inputFile string) int {
 	data := loadDaySixData(inputFile)
-
-	OBSTACLE := '#'
-	EMPTY := '.'
-	VISITED := 'X'
 
 	colMax := len(data[0]) - 1
 	rowMax := len(data) - 1
 
-	var currCol int
-	var currRow int
 	var nextCol int
 	var nextRow int
-	var direction rune
 	var nextRune rune
 
 	// Find starting position
-	found := false
-	for col := 0; col <= colMax; col++ {
-		if found {
-			break
-		}
-		for row := 0; row <= rowMax; row++ {
-			r := data[row][col]
-			if r != OBSTACLE && r != EMPTY {
-				found = true
-				currCol = col
-				currRow = row
-				direction = r
-				data[row][col] = VISITED
-				break
-			}
-		}
-	}
+	currCol, currRow, direction := startingPosition(data)
+	data[currRow][currCol] = VISITED
 
 	// Walk and Mark
 	for {
-		if direction == '^' {
-			nextRow = currRow - 1
-			nextCol = currCol
-		}
-		if direction == '>' {
-			nextRow = currRow
-			nextCol = currCol + 1
-		}
-		if direction == 'v' {
-			nextRow = currRow + 1
-			nextCol = currCol
-		}
-		if direction == '<' {
-			nextRow = currRow
-			nextCol = currCol - 1
-		}
+		nextRow, nextCol = nextPosition(direction, currRow, currCol)
 
 		if 0 > nextCol || nextCol > colMax || 0 > nextRow || nextRow > rowMax {
 			break
@@ -90,55 +57,69 @@ func solvePart1(inputFile string) int {
 	return sum
 }
 
-func rotateGuard(direction rune) rune {
+func rotateGuard(direction rune) (newDirection rune) {
 	switch direction {
 	case '^':
-		direction = '>'
+		newDirection = '>'
 	case '>':
-		direction = 'v'
+		newDirection = 'v'
 	case 'v':
-		direction = '<'
+		newDirection = '<'
 	case '<':
-		direction = '^'
+		newDirection = '^'
 	default:
 		log.Fatal("unknown direction")
 	}
 
-	return direction
+	return newDirection
+}
+
+func nextPosition(direction rune, currRow int, currCol int) (nextRow int, nextCol int) {
+	if direction == '^' {
+		nextRow = currRow - 1
+		nextCol = currCol
+	}
+	if direction == '>' {
+		nextRow = currRow
+		nextCol = currCol + 1
+	}
+	if direction == 'v' {
+		nextRow = currRow + 1
+		nextCol = currCol
+	}
+	if direction == '<' {
+		nextRow = currRow
+		nextCol = currCol - 1
+	}
+	return nextRow, nextCol
+}
+
+func startingPosition(grid [][]rune) (int, int, rune) {
+	colMax := len(grid[0]) - 1
+	rowMax := len(grid) - 1
+
+	for col := 0; col <= colMax; col++ {
+		for row := 0; row <= rowMax; row++ {
+			r := grid[row][col]
+			if r != OBSTACLE && r != EMPTY {
+				return col, row, r
+			}
+		}
+	}
+
+	log.Fatal("No starting position found")
+	return 0, 0, EMPTY
 }
 
 func solvePart2(inputFile string) int {
 	data := loadDaySixData(inputFile)
 
-	OBSTACLE := '#'
-	EMPTY := '.'
+	sum := 0
+
+	startCol, startRow, startDirection := startingPosition(data)
 
 	colMax := len(data[0]) - 1
 	rowMax := len(data) - 1
-
-	sum := 0
-
-	var startCol int
-	var startRow int
-	var startDirection rune
-
-	// Find starting position
-	found := false
-	for col := 0; col <= colMax; col++ {
-		if found {
-			break
-		}
-		for row := 0; row <= rowMax; row++ {
-			r := data[row][col]
-			if r != OBSTACLE && r != EMPTY {
-				found = true
-				startCol = col
-				startRow = row
-				startDirection = r
-				break
-			}
-		}
-	}
 
 	for c := 0; c <= colMax; c++ {
 		for r := 0; r <= rowMax; r++ {
@@ -162,27 +143,7 @@ func solvePart2(inputFile string) int {
 			loop := false
 			// Walk
 			for {
-				if rotations >= 3 {
-					sum++
-					break
-				}
-
-				if direction == '^' {
-					nextRow = currRow - 1
-					nextCol = currCol
-				}
-				if direction == '>' {
-					nextRow = currRow
-					nextCol = currCol + 1
-				}
-				if direction == 'v' {
-					nextRow = currRow + 1
-					nextCol = currCol
-				}
-				if direction == '<' {
-					nextRow = currRow
-					nextCol = currCol - 1
-				}
+				nextRow, nextCol = nextPosition(direction, currRow, currCol)
 
 				if 0 > nextCol || nextCol > colMax || 0 > nextRow || nextRow > rowMax {
 					break
@@ -191,6 +152,10 @@ func solvePart2(inputFile string) int {
 				nextRune = data[nextRow][nextCol]
 				if nextRune == '#' {
 					rotations++
+					if rotations >= 3 {
+						sum++
+						break
+					}
 					direction = rotateGuard(direction)
 					continue
 				}
@@ -199,13 +164,12 @@ func solvePart2(inputFile string) int {
 
 				for _, r := range track[currRow][currCol] {
 					if direction == r {
+						sum++
 						loop = true
 						break
 					}
 				}
-
 				if loop {
-					sum++
 					break
 				}
 
