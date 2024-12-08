e50716a7abe66d26bd9d60515e0edd0e19cdefca
Michael Järvå <michael.jarva@gmail.com>
2024-12-08 07:32:55 +0100

    day8

---
+++ day08/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func solvePart1(inputFile string) int {
+	//Load data
+
+	//Map all locations for each antenna type
+
+	//For each antenna type, use location list to place antinodes for each pair
+	//Antinode location is the x/y-offset between the two antennas added/subtracted to an antenna
+	return 0
+}
+
+// func solvePart2(inputFile string) int {
+// 	return 0
+// }
+
+func loadDaySevenData(inputFile string) {
+
+}
+
+func main() {
+	fmt.Println(solvePart1("input1.txt"))
+	// fmt.Println(solvePart2("input1.txt"))
+}
